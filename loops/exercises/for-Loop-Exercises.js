0498f05f1db04e0dec21b3abec461b946f1f3194
arunastanley <arunapaulus@gmail.com>
2024-06-23 15:45:22 -0500

    Loops Exercises

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,66 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+//a
+for(i = 0; i <= 20; i++)
+  {
+    console.log(i);
+  }
 
+//b
+for(i =3; i <= 29; i=i+2)
+  {
+    console.log(i);
+  }
 
+//c
+  for(i=12; i>=-14; i=i-2)
+    {
+      console.log(i);
+    }
 
+//d
+ for(i=50; i >=20; i--)
+  if(i%3 === 0){
+    console.log(i);
+  }
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
-
+let course = "LaunchCode";
+let arrays = [1, 5, 'LC101', 'blue', 42];
+//a
+for(i=0; i<arrays.length; i++)
+  {
+    console.log(arrays[i]);
+  }
+//b
+for(i = course.length-1; i>=0; i--)
+  {
+    console.log(course[i]);
+  }  
 
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  let arr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [];
+  let odds = [];
+   for(i=0; i < arr.length; i++)
+    {
+      if(arr[i]%2 === 0){
+        evens.push(arr[i]);
+    }else{
+        odds.push(arr[i]);
+    }
+  }
+  console.log(`evens: ${evens}`);
+  console.log(`odds: ${odds}`);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,48 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
-
-
+let fuelLevel = 0;
+let noOfAstronauts = 0;
+let shuttleAlt = 0;
+  const input = require('readline-sync');
 
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
-
+  fuelLevel= Number(input.question("Enter the starting fuel level: "));
+  while(fuelLevel < 5000 || fuelLevel >30000)
+    { 
+      fuelLevel= Number(input.question("Enter a positive no. between 5000 and 30000: "));
+    }
+    console.log ("starting Fuel Level= ", fuelLevel);
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  
-  
+  noOfAstronauts = Number(input.question("Enter the no. of astronauts: "));
+
+  while(noOfAstronauts < 1 || noOfAstronauts > 7)
+    {
+      noOfAstronauts = Number(input.question("Astronaut count should be between 1 and 7:  "));
+    }
+  console.log('No of astronauts = ', noOfAstronauts)
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
+while (fuelLevel - (noOfAstronauts*100) > 0 )
+  {
+    fuelLevel = fuelLevel - (noOfAstronauts*100);
+    shuttleAlt= shuttleAlt + 50;
+    console.log("Fuel: ",fuelLevel);
+    console.log("Alt: ",shuttleAlt);
+  }
 
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
-
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+console.log(`The shuttle gained an altitude of ${shuttleAlt} km`);
+if (shuttleAlt >= 2000){
+  console.log(`Orbit achieved!`);
+}else{
+  console.log(`Failed to reach orbit`);
+}
