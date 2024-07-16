ea9e1242a1a6881af0d3ab378094526db729f6ff
arunastanley <arunapaulus@gmail.com>
2024-07-15 20:26:31 -0500

    Unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,61 @@
-
+let message = {
+    '011': 'Launch!',
+    '101': 'Code!',
+    '110': 'Rocks!',
+    '001': 'LaunchCode!',
+    '100': 'Code Rocks!',
+    '010': 'Launch Rocks! (CRASH!!!!)',
+    '000': 'LaunchCode Rocks!',
+    '111': "Rutabagas! That doesn't work."
+};
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development","Data Analysis","Liftoff"],
+    launchOutput: function(num){
+
+        let numArray = [2,3,5];
+        let remainderArray = [];
+
+        for(let i=0; i <numArray.length ; i++){
+            if(num % numArray[i] === 0){
+            remainderArray[i] = 0;
+            }else{
+                remainderArray[i] = 1;
+            }
+            
+        }
+        return message[remainderArray.join('')];
+        // if(num % 2 === 0 && num % 3=== 0 && num % 5 ===0){
+        //     return 'LaunchCode Rocks!';
+        // }
+
+        // if(num % 2 === 0 && num % 5 ===0){
+        //     return 'Launch Rocks! (CRASH!!!!)';
+        // }
+
+        // if(num % 3 === 0 && num % 5 ===0){
+        //     return 'Code Rocks!';
+        // }
+
+        // if(num % 2 === 0 && num % 3 ===0){
+        //     return 'LaunchCode!';
+        // }
+        //     if(num % 2 === 0){
+        //         return 'Launch!';
+        //     }
+        //     if(num % 3 === 0){
+        //         return 'Code!';
+        //     }
+        //     if(num % 5 === 0){
+        //         return 'Rocks!';
+        //     }
+        //     else{
+        //         return "Rutabagas! That doesn't work.";
+        //     }
+
+    }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,61 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+    test("should have organization as 'non profit'",function(){
+          expect(launchcode.organization).toBe("nonprofit");
+    });
+
+    test("should have executiveDirector as 'Jeff'",function(){
+      expect(launchcode.executiveDirector).toBe("Jeff");
 });
+
+    test("should have percentageCoolEmployees as 100",function(){
+      expect(launchcode.percentageCoolEmployees).toBe(100);
+    });
+
+
+    test("should return an array of values",function(){
+      expect(launchcode.programsOffered.includes("Web Development")).toBeTruthy();
+      expect(launchcode.programsOffered.includes("Data Analysis")).toBeTruthy();
+      expect(launchcode.programsOffered.includes("Liftoff")).toBeTruthy();
+      expect(launchcode.programsOffered.length).toBe(3);
+    });
+
+
+    test("should return 'Launch!' when num div by only 2",function(){
+        expect(launchcode.launchOutput(2)).toBe('Launch!');
+    });
+
+    test("should return 'Code!' when num div by only 3",function(){
+      expect(launchcode.launchOutput(9)).toBe('Code!');
+    }); 
+
+    test("should return 'Code!' when num div by only 3",function(){
+      expect(launchcode.launchOutput(9)).toBe('Code!');
+  });
+
+  test("should return 'Rocks!' when num div by only 5",function(){
+    expect(launchcode.launchOutput(25)).toBe('Rocks!');
+});
+
+test("should return 'LaunchCode!' when num div 2 and 3",function(){
+  expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+});
+
+test("should return 'Code Rocks!' when num div 3 and 5",function(){
+  expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+});
+
+test("should return 'Launch Rocks! (CRASH!!!!)' when num div 2 and 5",function(){
+  expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+});
+
+test("should return 'LaunchCode Rocks!' when num div 2, 3 and 5",function(){
+  expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+});
+
+test("should return 'Rutabagas! That doesn't work.' when num not div by 2, 3 and 5",function(){
+  expect(launchcode.launchOutput(31)).toBe("Rutabagas! That doesn't work.");
+});
+  
+})
