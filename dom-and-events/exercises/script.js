1f9a9dda15912083ced9a225df1df7e1a1911436
arunastanley <arunapaulus@gmail.com>
2024-08-05 14:16:49 -0500

    DOM and Events exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click",function(event){
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover",function(event){
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout",function(event){
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function(event){
+        let confirmation = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmation){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+
+    });
     
 }
 
