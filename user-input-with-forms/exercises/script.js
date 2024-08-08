933c4d595bf3b855df89396f94867cd319c3014e
arunastanley <arunapaulus@gmail.com>
2024-08-08 18:42:15 -0500

    Forms Exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,20 @@
 //Code Your Solution Below
+window.addEventListener("load",function(){
+    let nameOftest = document.querySelector("input[name=testName]")
+    let date = document.querySelector("input[name=testDate]")
+    let typeOfRocket = document.querySelector("select[name=rocketType]")
+    let noOfBooster = document.querySelector("input[name=boosterCount]")
+    let windRating = document.querySelector("input[name=windRating]")
+    let prodGradeServers = document.querySelector("input[name=productionServers]")
+    let form = document.querySelector("form");
+      form.addEventListener("submit", function(event) {
+          if (nameOftest.value === "" || date.value === "" || typeOfRocket.value === "" || 
+              noOfBooster.value === "" || prodGradeServers.value === ""){
+              alert("All fields are required");
+               event.preventDefault();
+             
+          }
+        
+        
+      })
+  })
